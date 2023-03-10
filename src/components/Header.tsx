dd0cc41702c2c62d8aeb4055e1a0679c29edf6ff
Luki Klekner <lukaszklekner@gmail.com>
2023-03-10 08:16:36 +0100

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => (
+    <>
+        <h1>Santa app</h1>
+        <ul>
+            <li><Link to='/gift'>Gitfs</Link></li>
+            <li><Link to='/children'>Children</Link></li>
+        </ul>
+        <hr />
+    </>
+)
