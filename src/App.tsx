24d34d261a0f8d4d3c72a1007191968f36ae565c
Luki Klekner <lukaszklekner@gmail.com>
2023-03-10 08:43:38 +0100

    feat: install and configure react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
 import { GiftView } from './views/GiftView';
+import { NotfoundView } from './views/NotFoundView';
 
 function App() {
   return (
     <div className="App">
-      <GiftView />
+      <Header />
+      <Routes>
+        <Route path="/gift" element={<GiftView />} />
+        <Route path="*" element={<NotfoundView />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/views/NotFoundView.tsx
@@ -0,0 +1,3 @@
+export const NotfoundView = () => (
+    <p><strong>Page not found! :-(</strong></p>
+)
